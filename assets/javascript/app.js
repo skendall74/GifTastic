a87af70243a1a3af66f8675b36569362620382a0
skendall <shannonckendall@gmail.com>
2019-01-06 13:01:30 -0500

    One more fix to add pics replace eachother.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,7 +21,7 @@ $("#addTeam").on("click", function(){
 	var team = $("#team-input").val().trim();
 	// that input is now added to the array
 	teams.push(team);
-	// the makeButtons function is called, which makes buttons for all my teams plus the user team
+	// the makeButtons function is called, which makes buttons for all my teams plus the user teams
 	makeButtons();
 	// this line is so users can hit "enter" instead of clicking the submit button
 	return false; 
@@ -36,24 +36,23 @@ function displayGifs(){
 		$.ajax({url: queryURL, method: "GET"}).done(function (response) {
 			console.log(response.data);
 			// save results as a variable
-			var results = response.data;
+            var results = response.data;
 			// for loop goes through each gif and adds these variables
 			for (var i = 0; i < results.length; i++) {
-				// creates a generic div to hold the results
+                // creates a generic div to hold the results
 				var gifDiv = $('<div class=gifs>');
 				var teamGif = $('<img>');
-				var teamGif = $('<img>');
 					teamGif.attr('src', results[i].images.fixed_height_still.url);
-					// teams the rating on hover
-					teamGif.attr('title', "Rating: " + results[i].rating);
+					// teams for animate hover
 					teamGif.attr('data-still', results[i].images.fixed_height_still.url);
 					teamGif.attr('data-state', 'still');
 					teamGif.addClass('gif');
-					teamGif.attr('data-animate', results[i].images.fixed_height.url);
-				gifDiv.append(teamGif)
-				// gifDiv.append(p)
+                    teamGif.attr('data-animate', results[i].images.fixed_height.url);
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                gifDiv.prepend(p);
+                gifDiv.prepend(teamGif)
 				$("#gifsView").prepend(gifDiv);
-			}
+            } 
 			
 		});
 }
